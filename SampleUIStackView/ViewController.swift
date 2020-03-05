536448c1bbf540e9c7b590f1a1e4cd115975f8b5
sakiyamaK <sakiyama.k@gmail.com>
2020-03-06 02:29:44 +0900

    a

---
+++ SampleUIStackView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleUIStackView
+//
+//  Created by sakiyamaK on 2020/03/05.
+//  Copyright © 2020 sakiyamaK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
